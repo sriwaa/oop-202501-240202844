7ef02492bcd24f344d11116ceedd8dac0c50af8a
sriwaa <sri610970@gmail.com>
2025-10-03 14:28:09 +0700

    upload tugas week 1

--- praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloOOP.java
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloOOP.java
@@ -1,3 +1,23 @@
-//
-kode
-HelloOOP.java
+package com.upb.agripos;
+
+// Paradigma OOP: buat class dan objek
+class Person {
+    String nama;
+    String nim;
+
+    Person(String nama, String nim) {
+        this.nama = nama;
+        this.nim = nim;
+    }
+
+    void sayHello() {
+        System.out.println("Hello World, I am " + nama + "-" + nim);
+    }
+}
+
+public class HelloOOP {
+    public static void main(String[] args) {
+        Person p = new Person("sriwaa", "240202844");
+        p.sayHello();
+    }
+}
--- praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloProcedural.java
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloProcedural.java
@@ -1,3 +1,9 @@
-//
-kode
-HelloProcedural.java
+package com.upb.agripos;
+
+public class HelloProcedural {
+    public static void main(String[] args) {
+        String nama = "sriwaa";
+        String nim = "240202844";
+        System.out.println("Hello World, I am " + nama + "-" + nim);
+    }
+}
